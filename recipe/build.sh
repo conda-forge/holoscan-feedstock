dd3ea2754ac375bfe18a8577f9e3bb58dc3634a4
Billy Suh <bsuh@nvidia.com>
2025-05-19 20:30:32 -0700

    edit per review comments

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,6 @@
 set -e
 set -x
 
-mkdir -p $PREFIX/NOTICE
-
 # Create the site-packages dir if it doesn't exist
 mkdir -p $SP_DIR
 echo "site-packages dir, SP_DIR = $SP_DIR"
@@ -17,7 +15,6 @@ rm -vr lib/ucx
 check-glibc bin/* lib/* lib/ucx/* lib/gxf_extensions/*
 find python/ -name "*.so*" | xargs -I"{}" check-glibc "{}"
 
-cp -v NOTICE $PREFIX/NOTICE
 cp -rv bin $PREFIX/
 cp -rv examples $PREFIX/
 cp -rv lib $PREFIX/
