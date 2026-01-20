6d3c5a861e6e4b4c3048bfd7bea273c37d596143
Alex Dibbley <adibbley@nvidia.com>
2026-01-20 08:08:14 -0800

    Convert to python only feedstock

--- recipe/bindings-build.sh
+++ recipe/bindings-build.sh
@@ -5,6 +5,8 @@ py=${PY_VER//./}
 
 python -m zipfile -e holoscan*-cp${py}-cp${py}-manylinux_*.whl holoscan
 cd holoscan
+cp holoscan*.dist-info/LICENSE.txt $SRC_DIR/LICENSE
+cp holoscan*.dist-info/NOTICE.txt $SRC_DIR/NOTICE
 rm -rvf holoscan*.data/purelib/holoscan/lib
 cp -rvp holoscan*.data/purelib/holoscan $SP_DIR/
 
